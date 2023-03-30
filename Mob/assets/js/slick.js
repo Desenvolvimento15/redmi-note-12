c1f2d2df673f31b2df28eec19c9208a069f8c81c
desenvolvimento15 <desenvolvimento15@dl.com.br>
2023-03-30 13:45:29 -0300

    vs01

---
+++ Mob/assets/js/slick.js
@@ -0,0 +1,15 @@
+
+$('.galeria').slick({
+    arrows: false,
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    });
+    
+    
+    
